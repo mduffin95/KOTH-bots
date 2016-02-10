6ada6960e99a807d4759e6c10888a57f92ec7940
Gavin Parker <gp14958@my.bristol.ac.uk>
2016-02-10 19:45:08 +0000

    Added test bot 'Crap'

---
+++ Crap.java
@@ -0,0 +1,15 @@
+package controller;
+import java.util.*;
+
+/**
+ * Created by gavin on 10/02/2016.
+ */
+public class Crap {
+    public static void main(String[] args){
+        //String newArgs[] = args[0].split(";");
+        //int round = Integer.parseInt(newArgs[0]);
+
+
+        System.out.println("N");
+    }
+}
